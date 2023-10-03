52caaf26045c4861ffb1188d5b49f8d1b0c0e8e2
uzairk241 <m.uzairkhan241@gmail.com>
2023-10-03 19:11:42 +0500

    first commit

--- routes/usertesting.js
+++ routes/usertesting.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router=express.Router()
 
-router.route("/user").post((req,res)=>{
+router.route("/user").post(async(req,res)=>{
     res.status(200).json({"message":"hello"})
 });
 
